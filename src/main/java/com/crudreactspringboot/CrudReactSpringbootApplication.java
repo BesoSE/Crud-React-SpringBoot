ae5a51f5c7a0771207d82ae6f448e8f4b04b093c
BesoSE <semso2612@gmail.com>
2021-02-25 21:58:52 +0100

    initial commit

---
+++ src/main/java/com/crudreactspringboot/CrudReactSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.crudreactspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudReactSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudReactSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crudreactspringboot/CrudReactSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudreactspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudReactSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
